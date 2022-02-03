67b189c683755fce77b76eb3d23c6175346bc36e
coldcookies <73250770+coldcookies@users.noreply.github.com>
2022-02-03 10:18:10 -0500

    function for updating board + prep for game ai

--- script/interface.js
+++ script/interface.js
@@ -1,6 +1,6 @@
-const InterfaceManager = {
-   init: () => {
-     let cells = document.querySelectorAll("#gameBoard > div");
+ const InterfaceManager = (() => {
+   let cells = document.querySelectorAll("#gameBoard > div");
+   const init = () => {
      for (let i = 0; i < cells.length; i++) {
        const j = i;
        cells[i].setAttribute("style", `
@@ -10,26 +10,35 @@ grid-row-start: ${i % 3 + 1}
 grid-row-end: ${i % 3 + 2}`);
        cells[i].addEventListener("click", function() {
          let currentPlayerIndex = Game.getTurn() % 2;
-         if ( /*currentPlayerIndex == 0 && */ Game.players[currentPlayerIndex].move(j)) {
-           this.textContent = Game.players[currentPlayerIndex].sym;
+         if (currentPlayerIndex == 0 && Game.players[currentPlayerIndex].move(j)) {
+           Opponent.play();
+           InterfaceManager.updateBoard();
          }
        });
      }
+     document.getElementById("gameBoard").addEventListener("click", InterfaceManager.updateDesc);
+
      document.getElementById("reset").addEventListener("click", function() {
        Game.clearGame();
        for (let i = 0; i < cells.length; i++) {
          cells[i].textContent = "";
        }
-       document.getElementById("desc").textContent = `${Game.players[Game.getTurn() % 2].sym}'s turn`;
+       document.getElementById("desc").textContent = `X's turn`;
        this.style.display = "none";
      });
-     document.getElementById("desc").textContent = `${Game.players[Game.getTurn() % 2].sym}'s turn`;
-     document.getElementById("gameBoard").addEventListener("click", InterfaceManager.updateDesc);
-   },
-   updateDesc: () => {
+
+     document.getElementById("desc").textContent = `X's turn`;
+   };
+
+   const updateBoard = () => {
+     for (let i = 0; i < cells.length; i++) {
+       cells[i].textContent = Game.getBoard()[i];
+
+     }
+   }
+   const updateDesc = () => {
      let isFin = Game.checkFin();
      let desc = document.getElementById("desc");
-     desc.textContent = `${Game.players[Game.getTurn() % 2].sym}'s turn`;
      switch (isFin.gameState) {
        case -1:
          desc.textContent = "Tie";
@@ -39,11 +48,12 @@ grid-row-end: ${i % 3 + 2}`);
          desc.textContent = `Winner: ${isFin.winner}`;
          document.getElementById("reset").style.display = "inline-block";
          break
-       case 0:
-         desc.textContent = `${Game.players[Game.getTurn() % 2].sym}'s turn`;
-         break
-
      }
    }
- }
+   return {
+     init,
+     updateBoard,
+     updateDesc
+   };
+ })();
  InterfaceManager.init();
