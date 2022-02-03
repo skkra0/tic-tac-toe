adc3a865cef0c076012de369c5ec00e3e4101d4e
coldcookies <73250770+coldcookies@users.noreply.github.com>
2022-02-03 10:38:13 -0500

    bug fix

--- script/opponent.js
+++ script/opponent.js
@@ -1,6 +1,6 @@
- const Opponent = {
+  const Opponent = {
    play: () => {
-     let possibleMoves = Game.getBoard().map((e, i) => e === '' ? i : null).filter(Number);
+     let possibleMoves = Game.getBoard().map((e, i) => !e ? i : null).filter(e => e !== null);
      let p = Math.floor(Math.random() * possibleMoves.length);
      Game.players[1].move(possibleMoves[p]);
    }
